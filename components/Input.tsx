1ad9cc71e59341413bae85484791853a26d20958
GitBerg <beguinhoboy@hotmail.com>
2024-10-01 23:43:09 -0300

    Finalizada a estrutura das páginas de Login e de Registro

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,9 +1,31 @@
-const Input = () => {
+import React from 'react'
+
+interface InputProps{
+    id:string
+    onChange: any
+    value: string
+    label: string
+    type?: string
+}
+
+
+const Input: React.FC<InputProps> = ({
+    id,
+    onChange,
+    value,
+    label,
+    type
+}) => {
     return(
-        <input 
+        <div className="relative">
+            <input 
+            id={id}
+            value={value}
+            type={type}
+            onChange={onChange}
             className="
                 block
-                roudend-md
+                rounded-md
                 px-6
                 pt-6
                 pb-1
@@ -18,6 +40,28 @@ const Input = () => {
             "
             placeholder=" "
         />
+        <label 
+        className="
+            absolute
+            text-md
+            text-zinc-400
+            duration-150
+            transform
+            -translate-y-3
+            scale-75
+            top-4
+            z-10
+            origin-[0]
+            left-6
+            peer-placeholder-shown:scale-100
+            peer-placeholder-shown:translate-y-0
+            peer-focus:scale-75
+            peer-focus:-translate-y-3
+        "
+        htmlFor={id}>
+            {label}
+        </label>
+        </div>     
     )
 }
 
